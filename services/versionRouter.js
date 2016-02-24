f5b48573059e4e96647f04b1ba4188f8a06b48b7
alexjab <alexandre.jablon@gmail.com>
2016-02-24 20:53:37 +0100

    Improve comments in versionRouter

--- services/versionRouter.js
+++ services/versionRouter.js
@@ -1,6 +1,14 @@
-// Inpired by: https://github.com/nswbmw/koa-version
+// Inspired by: https://github.com/nswbmw/koa-version
 // Main differences:
-// - relies on this.version, which can be set however we want. 
+//  - it relies on this.version (which can be set
+//    however we want), and not on any specific header,
+//    query or param.
+//  - it does not use semver or anything like it,
+//    and does simple string equality. In the case of an
+//    API version, I fail to see the point of using
+//    semver, since you probably don't want to roll
+//    tons of different versions, at the risk of
+//    confusing your audience.
 
 const koa = require('koa');
 const compose = require('koa-compose');
@@ -9,9 +17,7 @@ const versions = {};
 
 module.exports = function version(version, route, app) {
   const _app = koa();
-
   route(_app);
-
   versions[version] = _app.middleware;
 
   return function* (upstream) {
